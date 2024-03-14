43fe9541f324823136ae318ad48f39448f65017d
Vipul <vipuldamrepro26@gmail.com>
2024-03-14 19:13:50 +0530

    progress bar project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [num, setNum] = useState(0);
+
+  useEffect(() => {
+
+    let numspan = document.querySelector('span');
+    let para = document.querySelector('p');
+
+    if(num < 100){
+
+      setTimeout(() => {
+        setNum(prev => prev += 1);
+        para.innerHTML = 'Loading...';
+      }, 100);
+  
+
+      if(num > 55){
+        numspan.style.color = 'white';
+      }
+    }
+
+    if(num === 100){
+      para.innerHTML = 'Completed!'
+      document.querySelector('.App').style.backgroundColor = 'cyan';
+    }
+    
+  },[num])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Progress Bar</h1>
+        <div className='main'>
+          <div style={{width:`${num}%`, height: '100%', backgroundColor: 'green', transition: 'width 0.5s'}}></div>
+          <span>{num}%</span>
+        </div>
+        <p></p>
     </div>
   );
 }
